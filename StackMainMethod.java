62a0dc0ee610fa7e21b6ab3af8e0973fb6d9fae9
vinit <vinitkkulkarni@gmail.com>
2023-04-23 23:26:23 +0530

    Added:code to call methods of stack

---
+++ StackMainMethod.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.stackusinglinkedlist;
+
+public class StackMainMethod {
+    public static void main(String[] args) {
+        MyStack stack1 = new MyStack();
+
+        stack1.push(70);
+        stack1.push(30);
+        stack1.push(56);
+
+        stack1.printStack();
+
+        stack1.peak();
+        stack1.pop();
+        stack1.printStack();
+
+        stack1.peak();
+        stack1.pop();
+        stack1.printStack();
+
+        stack1.peak();
+        stack1.pop();
+
+        stack1.peak();
+        stack1.pop();
+    }
+}
